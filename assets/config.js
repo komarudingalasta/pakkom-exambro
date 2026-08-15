53ba9173397ab6e81cebcea03fab1efa2a2f68d3
komarudingalasta <komarudingalasta@gmail.com>
2026-08-15 12:24:04 +0700

    Add files via upload

--- assets/config.js
+++ assets/config.js
@@ -1,8 +1,1 @@
-window.FIREBASE_CONFIG={
- apiKey:"AIzaSyAca_RjBN6Kln_FGmQwy7jcfcaFXf_d93w",
- authDomain:"pakkom-exambro-643f6.firebaseapp.com",
- projectId:"pakkom-exambro-643f6",
- storageBucket:"pakkom-exambro-643f6.firebasestorage.app",
- messagingSenderId:"914928651464",
- appId:"1:914928651464:web:b3979ba413dcda6906e9fd"
-};
+window.FIREBASE_CONFIG={apiKey:"AIzaSyAca_RjBN6Kln_FGmQwy7jcfcaFXf_d93w",authDomain:"pakkom-exambro-643f6.firebaseapp.com",projectId:"pakkom-exambro-643f6",storageBucket:"pakkom-exambro-643f6.firebasestorage.app",messagingSenderId:"914928651464",appId:"1:914928651464:web:b3979ba413dcda6906e9fd"};
